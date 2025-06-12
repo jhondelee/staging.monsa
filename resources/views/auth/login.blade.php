4cde44407b3d96cb85f04f9c8ae5ae894e4cbd86
jhondelee <jhondelee@gmail.com>
2025-06-12 16:35:06 +0800

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
             <div>
 
                 <h1 class="logo-name">
-                    <h4>STAGING</h4>
+                    <h5>STAGING</h5>
                     <center>
 
                         <img alt="image" width="150" height="150" class="img-responsive " src="/img/temporary-logo.jpg"/>
